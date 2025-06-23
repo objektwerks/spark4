9e8458dbd7637d20e50023a8144f48c5094a91ac
objektwerks <objektwerks@runbox.com>
2025-06-23 09:01:31 -0400

    refactored spark instance

--- src/main/scala/objektwerks/SparkInstance.scala
+++ src/main/scala/objektwerks/SparkInstance.scala
@@ -1,10 +1,13 @@
 package objektwerks
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
 
 object SparkInstance:
-  def create(): SparkSession =
-    SparkSession
+  def create(): (SparkSession, SparkContext) =
+    val session = SparkSession
       .builder()
       .master("local")
       .getOrCreate
+    val context = session.sparkContext
+    (session, context)
