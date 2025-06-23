88230d774f9214092bf5e232d4be21c8b23f5e5e
objektwerks <objektwerks@runbox.com>
2025-06-23 10:05:24 -0400

    added broadcast test

---
+++ src/test/scala/objektwerks/BroadcastTest.scala
@@ -0,0 +1,22 @@
+package objektwerks
+
+import munit.FunSuite
+
+import SparkInstance.*
+
+class BroadcastTest extends FunSuite:
+  test("broadcast variable"):
+    val keyValues = List( KeyValue(1, 1), KeyValue(2, 2), KeyValue(3, 3) )
+    val broadcastKeyValues = sparkContext.broadcast( keyValues )
+    assert( broadcastKeyValues.value.length == 3 )
+
+  test("broadcast join"):
+    val broadcastPersons = sparkContext.broadcast( sparkSession.read.json("./data/person/person.json").as[Person] )
+
+    val persons = broadcastPersons.value
+    val tasks = sparkSession.read.json("./data/task/task.json").as[Task]
+
+    val joinCondition = persons.col("id") === tasks.col("pid")
+    val personsTasks = persons.join(tasks, joinCondition)
+
+    assert( personsTasks.count == 4 )
