ce6ccbea6e48e85e851a67a2a3f1ba566123d23c
objektwerks <objektwerks@runbox.com>
2025-06-23 08:42:09 -0400

    reformatted acc test

--- src/test/scala/objektwerks/AccumulatorTest.scala
+++ src/test/scala/objektwerks/AccumulatorTest.scala
@@ -3,31 +3,28 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import scala.collection.JavaConverters._
+import scala.collection.JavaConverters.*
 
-import SparkInstance._
+final class AccumulatorTest extends AnyFunSuite with Matchers:
+  val sparkSession = SparkInstance.create()
+  val sparkContext = sparkSession.sparkContext
 
-class AccumulatorTest extends AnyFunSuite with Matchers {
-  test("long accumulator") {
+  test("long accumulator"):
     val longAcc = sparkContext.longAccumulator("longAcc")
     longAcc.add(1)
     longAcc.name.get shouldBe "longAcc"
     longAcc.value shouldBe 1
-  }
 
-  test("double accumulator") {
+  test("double accumulator"):
     val doubleAcc = sparkContext.doubleAccumulator("doubleAcc")
     doubleAcc.add(1.0)
     doubleAcc.name.get shouldBe "doubleAcc"
     doubleAcc.value shouldBe 1.0
-  }
 
-  test("collection accumulator") {
+  test("collection accumulator"):
     val intsAcc = sparkContext.collectionAccumulator[Int]("intsAcc")
     intsAcc.add(1)
     intsAcc.add(2)
     intsAcc.add(3)
     intsAcc.name.get shouldBe "intsAcc"
     intsAcc.value.asScala.sum shouldEqual 6
-  }
-}
