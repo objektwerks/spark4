06842720a4bca6081afd8d0d295cea57b5eac1b9
objektwerks <objektwerks@runbox.com>
2025-06-24 15:05:07 -0400

    edited spark instance

--- src/main/scala/objektwerks/SparkInstance.scala
+++ src/main/scala/objektwerks/SparkInstance.scala
@@ -6,12 +6,11 @@ import org.apache.spark.sql.SparkSession
 object SparkInstance:
   private val logger = LogManager.getLogger(SparkInstance.getClass)
 
-  val sparkSession =
-    SparkSession
-      .builder()
-      .master("local[*]")
-      .appName("spark-app")
-      .getOrCreate
+  val sparkSession = SparkSession
+    .builder()
+    .master("local[*]")
+    .appName("spark-app")
+    .getOrCreate
   val sparkContext = sparkSession.sparkContext
   logger.info("*** Initialized Spark Session instance.")
 
