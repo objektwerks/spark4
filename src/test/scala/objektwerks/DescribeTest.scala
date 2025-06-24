5956868612f9fae06416923639e33ea36d0f9012
objektwerks <objektwerks@runbox.com>
2025-06-24 15:02:11 -0400

    edited describe test

--- src/test/scala/objektwerks/DescribeTest.scala
+++ src/test/scala/objektwerks/DescribeTest.scala
@@ -8,6 +8,12 @@ import SparkInstance.*
   
 final class DescribeTest extends FunSuite:
   test("describe"):
-    val persons = sparkSession.read.json("./data/person.json").as[Person].cache
+    val persons = sparkSession
+      .read
+      .json("./data/person.json")
+      .as[Person]
+      .cache
     assert( persons.count == 4 )
-    persons.describe("id", "age", "name", "role").show()
+    persons
+      .describe("id", "age", "name", "role")
+      .show()
