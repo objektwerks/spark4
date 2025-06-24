b5ade348ae48832a80240811c1409be0220e34af
objektwerks <objektwerks@runbox.com>
2025-06-24 15:03:04 -0400

    edited explain test

--- src/test/scala/objektwerks/ExplainTest.scala
+++ src/test/scala/objektwerks/ExplainTest.scala
@@ -8,8 +8,15 @@ import SparkInstance.*
 
 final class ExplainTest extends FunSuite:
   test("explain"):
-    val persons = sparkSession.read.json("./data/person.json").as[Person].cache
-    val fred = persons.map(_.name.toUpperCase).filter(_ == "FRED").cache
+    val persons = sparkSession
+      .read
+      .json("./data/person.json")
+      .as[Person]
+      .cache
+    val fred = persons
+      .map(_.name.toUpperCase)
+      .filter(_ == "FRED")
+      .cache
     assert( fred.count == 1 )
     assert( fred.head == "FRED" )
     fred.explain(extended = true)
